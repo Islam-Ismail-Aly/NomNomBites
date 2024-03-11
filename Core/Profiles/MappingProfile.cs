918edd10a26b8cd241db5f34960a6c7d7d7d1598
Islam Ismail <intern.islam.ismail@arpuplus.com>
2024-03-12 01:14:59 +0200

    Add project files.

---
+++ Core/Profiles/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Core.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             : base(options)
+        {
+        }
+    }
+}
